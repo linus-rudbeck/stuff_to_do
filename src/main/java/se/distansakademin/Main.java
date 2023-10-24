7b4eadf03e2d6cb08625de04f4286735853240ad
linus-rudbeck <linus.rudbeck@distansakademin.se>
2023-10-24 10:50:45 +0200

    Initiate project

---
+++ src/main/java/se/distansakademin/Main.java
@@ -0,0 +1,9 @@
+package se.distansakademin;
+
+public class Main {
+    public static void main(String[] args) {
+        for (String url : args) {
+            System.out.println(url);
+        }
+    }
+}
